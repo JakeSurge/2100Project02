df1ccc01fa8f6f0521e13bcad8eadd8c60d402dc
Jake Surgeon <jakobsurge@gmail.com>
2023-11-23 01:42:04 -0500

    Commented out some reference code for getting key value pairs from the json objects in a json file

--- import_data.py
+++ import_data.py
@@ -21,17 +21,18 @@ def add_item(id, name, stackibility, attack_speed, attack_damage, peaceful_obtai
     connection.commit()
     cursor.close()
 
-#open file if exists
-with open('itemlist.json', 'r') as f:
-    file = json.load(f)         #python list = to json file
+# OPENING FILE EXAMPLE
+# #open file if exists
+# with open('itemlist.json', 'r') as f:
+#     file = json.load(f)         #python list = to json file
     
-    #use for loop to iterate through each json object in the json file
-    for i in file:
-        #convert each json object to a python dictionary
-        item = dict(i)
-        #iterate through the key value pairs of each object
-        for key, value in item.items():
-            print (value)
+#     #use for loop to iterate through each json object in the json file
+#     for i in file:
+#         #convert each json object to a python dictionary
+#         item = dict(i)
+#         #iterate through the key value pairs of each object
+#         for key, value in item.items():
+#             print (value)
         
 try:
     connection = pg.connect('dbname=minecraft_items user=postgres password=Ilikepie13$ port=5432')      #connection to minecraft_items database
