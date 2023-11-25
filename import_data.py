48806c98c920b7cbacad1b1eb7ae880387c15524
Jake Surgeon <jakobsurge@gmail.com>
2023-11-24 22:00:39 -0500

    Made other csvs for other items that need to be added. Have code and I think csv done for normal potions but need to double check. Have most of splash done but really need to double check with in game values since the source I was using was bad. Then I need to do the stuff for lingering and for tipped arrows. After I do this the main table should be done but then I will need to do all the other tables which should not take as long.

--- import_data.py
+++ import_data.py
@@ -1,12 +1,9 @@
 import psycopg as pg
 import json
+import csv
 
 def add_item(id, name, stackability, attack_speed, attack_damage, peaceful_obtainable, renewable):
     cursor = connection.cursor()            #cursor for db connection
-
-    #change stackability value if it is "Unstackable"
-    if stackability.lower() == 'unstackable':
-        stackiblity = 1
 
     #string variable for the actual insert sql statement
     sql = """
@@ -17,6 +14,23 @@ def add_item(id, name, stackability, attack_speed, attack_damage, peaceful_obtai
 
     #execute sql statment with passed parameters
     cursor.execute(sql, (id, name, stackability, attack_speed, attack_damage, peaceful_obtainable, renewable))
+
+    #commit changes and close cursor
+    connection.commit()
+    cursor.close()
+
+def add_potion(id, name, peaceful_obtainable):
+    cursor = connection.cursor()            #cursor for db connection
+
+    #string variable for the actual insert sql statement
+    sql = """
+        INSERT INTO items (item_id, item_name, stackability, attack_speed, attack_damage, peaceful_obtainable, renewable)
+        VALUES (%s, %s, 1, 4, 1, %s, true)
+        ON CONFLICT DO NOTHING;
+          """
+    
+    #execute sql statment with passed parameters
+    cursor.execute(sql, (id, name, peaceful_obtainable))
 
     #commit changes and close cursor
     connection.commit()
@@ -38,7 +52,7 @@ def add_item(id, name, stackability, attack_speed, attack_damage, peaceful_obtai
 try:
     connection = pg.connect('dbname=minecraft_items user=postgres password=Ilikepie13$ port=5432')      #connection to minecraft_items database
 
-    #open file if exists
+    #open file if exists to add most of items to items table
     with open('items.json', 'r') as f:
         file = json.load(f)         #python list = to json file
         
@@ -63,6 +77,46 @@ try:
             
             #use add_item to add item information to db
             add_item(id, name, stackability, attack_speed, attack_damage, peaceful_obtainable, renewable)
+
+    #open files if exists to add potion items to items table
+    with open('potions.csv', 'r') as f:
+        file = csv.reader(f)
+
+        next(file)
+
+        #for loop that goes through each row of the csv and 
+        #pushes values as parameters to function that adds them to the db
+        for potion in file:
+            if potion[0]:
+                id = potion[0]
+                name = potion[1]
+                #assign peaceful_obtainable based off of the type of potion
+                if ("Healing" in potion[1]) or ("Fire Resistance" in potion[1]) or ("Weakness" in potion[1]) or ("Invisibility" in potion[1]) or ("Water" in potion[1]):
+                    peaceful_obtainable = True
+                else:
+                    peaceful_obtainable = False
+                #call function to add the potions to the items table
+                add_potion(id, name, peaceful_obtainable)
+    
+    #open files if exists to add potion items to items table
+    with open('splash_potions.csv', 'r') as f:
+        file = csv.reader(f)
+
+        next(file)
+
+        #for loop that goes through each row of the csv and 
+        #pushes values as parameters to function that adds them to the db
+        for potion in file:
+            if potion[0]:
+                id = potion[0].split("minecraft:")[0]
+                name = potion[1]
+                #assign peaceful_obtainable based off of the type of potion
+                if ("Fire Resistance" in potion[1]) or ("Water" in potion[1]):
+                    peaceful_obtainable = True
+                else:
+                    peaceful_obtainable = False
+                #call function to add the potions to the items table
+                #add_splash_potion(id, name, peaceful_obtainable)
 finally:
     if connection:
         connection.close()
