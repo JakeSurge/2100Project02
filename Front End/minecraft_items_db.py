bd0a3ccf78221868ca7f84cc4a0494ed3aa7dbee
Jake Surgeon <jakobsurge@gmail.com>
2023-12-06 00:07:12 -0500

    Got something started for the python front end. Getting some annying spacing errors in the decision logic and some others because psycopg is not on desktop but I need to look at it on my laptop.

---
+++ Front End/minecraft_items_db.py
@@ -0,0 +1,74 @@
+import psycopg as pg
+
+def select_view(view):
+    cursor = connection.cursor()            #cursor for db connection
+
+    #string variable for the select statement
+    cmd = sql.SQL("SELECT {} FROM {}";)\
+            .format(sql.Identifier("*"),
+                    sql.Placeholder())
+
+    #execute the select statement
+    cursor.execute(cmd, (view, ))
+
+    #take the output
+    output = cursor.fetchall()
+
+    #do something with output?
+    print(output)
+
+    #close the cursor
+    cursor.close()
+
+try:
+    #variable for help message in program to give command instructions
+    HELP_MESSAGE = """"""
+
+    #dictionary of the different views partnered with their command counterpart
+    VIEW_DICT = {
+        "default" : "public.default",
+        "breaking speeds" : "public.breaking_speeds_view",
+        "food effects" : "public.food_effects_view",
+        "smelting obtainable" : "public.smelting_obtainable_view",
+        "smeltable items" : "public.smeltable_items_view",
+        "fuel duration" : "public.fuel_duration_view",
+        "food items" : "public.food_items_view",
+        "cooldown" : "public.cooldown_view"
+    }
+
+    connection = pg.connect('dbname=minecraft_items user=postgres password=Ilikepie13$ port=5432')      #connection to minecraft_items database
+
+    #display help message
+    print(HELP_MESSAGE)
+
+    #start while loop to prompt user until the quit
+    while ():
+        #prompt for input
+        query_type = input("Select either 'preset' or 'custom' to start querying:").lower()
+
+        #decision logic for query type and onward
+        if query_type == "preset":
+            #prompt user again for what view
+            view_type = input("Select what view you want to see (default, breaking speeds, food effects, smelting obtainable, smeltable items, fuel duration, food items, cooldown):").lower()
+
+            #check for view
+            try:
+                #translate view command to actual view value
+                view = VIEW_DICT[view_type]
+
+                #pass view value to select_view function
+                select_view(view)
+            except:
+                #send error message
+                print("ERROR INCORRECT INPUT!!! TRY AGAIN!!!")
+
+        elif query_type == "custom":
+            #work on this later
+        elif query_type == "quit" or query_type == "exit":
+            break
+        else:
+            print("ERROR INCORRECT INPUT!!! TRY AGAIN!!!")
+
+finally:
+    if connection:
+        connection.close()
