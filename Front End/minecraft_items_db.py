22249a9914ae302a287613818f81e046f49d96a1
Jake Surgeon <jakobsurge@gmail.com>
2023-12-07 00:29:58 -0500

    Got a basic formatting done now for the output and added a little more error consideration. May do some minor adjustments so it typically formats better with the ID sections of the header. For now it is pretty acceptable though.

--- Front End/minecraft_items_db.py
+++ Front End/minecraft_items_db.py
@@ -1,7 +1,7 @@
 # import psycopg as pg
 from psycopg import connect, sql
 
-def select_view(view: str, search_attr: str, search_str: str, order_type: str):
+def select_view(view: str, search_attr: str, search_str: str, order_type: str, attributes):
     # first double check order_type to prevent injection
     if order_type != "ASC" and order_type != "DESC":
         raise Exception("ERROR INCORRECT ORDER TYPE! SQL INJECTION ATTEMPT DETECTED!")
@@ -23,22 +23,45 @@ def select_view(view: str, search_attr: str, search_str: str, order_type: str):
     # take the output
     output = cursor.fetchall()
 
+    # check is there are no rows and return if so
+    if len(output) == 0:
+        print("The resulting query had 0 results.")
+        return
+
     # pass to paginate function
-    paginate_output(output)
+    paginate_output(output, attributes)
 
     # close the cursor
     cursor.close()
 
-def paginate_output(output):
+def paginate_output(output, attributes):
     row_amount = len(output)            # amount of rows in current output
+
+    # create variable early for set data type
+    header = str("| ")
+
+    # create header to print
+    for attribute in attributes:
+        header += (f"{attribute} | ")
+    
+    # print header
+    print(header)
 
     # use amount of rows to make for loop for pagination
     for i in range(row_amount):
-        # print current row
-        print(output[i])
+        # create variable early for set data type
+        row = str("| ")
+        
+        # format current row
+        for column in output[i]:
+            row += (f"{column} | ")
+        
+        # print row
+        print(row)
 
         # if at a row count of 10 prompt user to continue
         if (i + 1) % 10 == 0 and i != (row_amount - 1):
+            
             # user input to continue or not
             cont = input("Press ENTER to continue (input anything else to stop)...")
 
@@ -62,6 +85,23 @@ try:
         "cooldown": "cooldown_view"
     }
 
+    # dictionary of the attributes of every view
+    VIEW_ATTRIBUTES_DICT = {
+        "default": ["ID", "Name", "Stackability", "Attack Speed", "Attack Damage", "Damage Per Second", "Peaceful Obtainable", "Renewable", "Survival Obtainable"],
+        "breaking speeds": ["ID", "Name", "Breaking Type", "Breaking Speed"],
+        "food effects": ["ID", "Name", "Effect", "Effect Degree", "Time", "Chance"],
+        "smelting obtainable": ["ID", "Name", "Smelting Method (Obtained)"],
+        "smeltable items": ["ID", "Name", "XP From Smelting", "Smelting Method"],
+        "fuel duration": ["ID", "Name", "Fuel Duration"],
+        "food items": ["ID", "Name", "Hunger", "Saturation"],
+        "cooldown": ["ID", "Name", "Cooldown"]
+    }
+
+    # dictionary of all the attributes for the custom search
+    CUSTOM_ATTRIBUTES = {
+
+    }
+
     # connection to minecraft_items database
     connection = connect(
         'dbname=minecraft_items user=postgres password=Ilikepie13$ port=5432')
@@ -82,12 +122,13 @@ try:
             view_type = input(
                 "Select what view you want to see (default, breaking speeds, food effects, smelting obtainable, smeltable items, fuel duration, food items, cooldown): ").lower()
 
-            view = None         # declare view variable for later use so scope still applies
-
             # check for view
             try:
                 # translate view command to actual view value
                 view = VIEW_DICT[view_type]
+
+                # translate view command to table header
+                attributes = VIEW_ATTRIBUTES_DICT[view_type]
                 
             except BaseException:
                 # send error message
@@ -99,16 +140,11 @@ try:
 
             if search == "":
                 # enter default values for search and order
-                select_view(view, "item_id", "%", "ASC")
+                select_view(view, "item_id", "%", "ASC", attributes)
             else:
                 # split input up
                 search_terms = search.split(" ", 2)
-                    
-                # declare empty search variables for later use so scope applies
-                search_attr = None
-                search_str = None
-                order_type = None
-
+                
                 # check for enough inputs
                 try:
                     # setup variables from search_terms
@@ -135,7 +171,7 @@ try:
                     continue
 
                 # run select_view with search parameters
-                select_view(view, search_attr, search_str, order_type)
+                select_view(view, search_attr, search_str, order_type, attributes)
         
         # decision logic for custom queries
         elif query_type == "custom":
@@ -145,6 +181,12 @@ try:
         # break out of loop with proper command
         elif query_type == "exit" or query_type == "quit":
             break
+
+        # give error message for anything else
+        else:
+            print("ERROR UNREGISTERED COMMAND! TRY AGAIN")
+
+# use finally to close connection
 finally:
     if connection:
         connection.close()
